6b6400bd43e69e5d3583c9be7164da51f58f19c8
Mario Santos <mariocsants@gmail.com>
2019-07-04 21:02:28 -0300

    Installing electron

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React and electron 👊.
         </p>
         <a
           className="App-link"
